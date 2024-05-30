642973078b83c4a3e99ad07ca76bb7a677dc9f4c
worada.sir <worada.sir@axonstech.com>
2024-05-30 20:16:47 +0700

    add mock blog for test

---
+++ a-board-ui/constants/mock.ts
@@ -0,0 +1,86 @@
+export const mock_blog: any[] = [
+  {
+    blogID: 1,
+    username: "Wittawat",
+    community: "History",
+    title: "History",
+    descriptions:
+      "The afterlife sitcom The Good Place comes to its culmination, the show’s two protagonists, Eleanor and Chidi, contemplate their future. Having lived thousands upon thousands of lifetimes together, and having experienced virtually everything this life has to offer, they are weary. It is time for it all to end. The show’s solution to this perpetual happiness-cum-weariness is extinction. When you have had enough, when you are utterly sated by love and joy and pleasure, you can walk through a passage to nothingness. And Chidi has had enough.",
+    createDate: "2024-05-27T17:00:00Z",
+    updateDate: "2024-05-27T17:00:00Z",
+    comments: [
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+    ],
+  },
+  {
+    blogID: 2,
+    username: "Tomato",
+    community: "History",
+    title: "History",
+    descriptions:
+      "The afterlife sitcom The Good Place comes to its culmination, the show’s two protagonists, Eleanor and Chidi, contemplate their future. Having lived thousands upon thousands of lifetimes together, and having experienced virtually everything this life has to offer, they are weary. It is time for it all to end. The show’s solution to this perpetual happiness-cum-weariness is extinction. When you have had enough, when you are utterly sated by love and joy and pleasure, you can walk through a passage to nothingness. And Chidi has had enough.",
+    createDate: "2024-05-27T17:00:00Z",
+    updateDate: "2024-05-27T17:00:00Z",
+    comments: [
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+    ],
+  },
+  {
+    blogID: 3,
+    username: "Milo",
+    community: "History",
+    title: "History",
+    descriptions:
+      "The afterlife sitcom The Good Place comes to its culmination, the show’s two protagonists, Eleanor and Chidi, contemplate their future. Having lived thousands upon thousands of lifetimes together, and having experienced virtually everything this life has to offer, they are weary. It is time for it all to end. The show’s solution to this perpetual happiness-cum-weariness is extinction. When you have had enough, when you are utterly sated by love and joy and pleasure, you can walk through a passage to nothingness. And Chidi has had enough.",
+    createDate: "2024-05-27T17:00:00Z",
+    updateDate: "2024-05-27T17:00:00Z",
+    comments: [
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+    ],
+  },
+  {
+    blogID: 4,
+    username: "Vilo",
+    community: "History",
+    title: "History",
+    descriptions:
+      "The afterlife sitcom The Good Place comes to its culmination, the show’s two protagonists, Eleanor and Chidi, contemplate their future. Having lived thousands upon thousands of lifetimes together, and having experienced virtually everything this life has to offer, they are weary. It is time for it all to end. The show’s solution to this perpetual happiness-cum-weariness is extinction. When you have had enough, when you are utterly sated by love and joy and pleasure, you can walk through a passage to nothingness. And Chidi has had enough.",
+    createDate: "2024-05-27T17:00:00Z",
+    updateDate: "2024-05-27T17:00:00Z",
+    comments: [
+      {
+        username: "Wittawat",
+        comments:
+          "Lorem ipsum dolor sit amet consectetur. Purus cursus vel est a pretium quam imperdiet. Tristique auctor sed semper nibh odio iaculis sed aliquet. Amet mollis eget morbi feugiat mi risus eu. Tortor sed sagittis convallis auctor.",
+        createDate: "2024-05-27T17:00:00Z",
+      },
+    ],
+  },
+];
