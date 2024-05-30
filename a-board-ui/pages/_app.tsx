ad3df46affe46f103fd71f053771035f14400772
worada.sir <worada.sir@axonstech.com>
2024-05-30 20:20:08 +0700

    update navbar to app

--- a-board-ui/pages/_app.tsx
+++ a-board-ui/pages/_app.tsx
@@ -1,6 +1,15 @@
+import Navbar from "@/components/layout/navbar";
 import "@/styles/globals.css";
+import { Flex } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Flex direction={"column"}>
+      <Navbar />
+      <Flex direction={"column"}>
+        <Component {...pageProps} />
+      </Flex>
+    </Flex>
+  );
 }
