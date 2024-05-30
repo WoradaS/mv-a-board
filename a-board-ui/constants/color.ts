4f9cd48690a99d7b30e32f771078fb823189e4d1
worada.sir <worada.sir@axonstech.com>
2024-05-30 20:16:05 +0700

    add color them

---
+++ a-board-ui/constants/color.ts
@@ -0,0 +1,12 @@
+export enum BrandColor {
+  Green500 = "#243831",
+  Green300 = "#2B5F44",
+  Green100 = "#D8E9E4",
+  Golden = "#C5A365",
+  Black = "#000000",
+  White = "#FFFFFF",
+  Text = "#191919",
+  Grey100 = "#BBC2C0",
+  Grey300 = "#939494",
+  Success = "#49A569",
+}
